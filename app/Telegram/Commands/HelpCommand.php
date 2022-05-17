084428d91ee896ed1678760f65d87a137806aef1
amrreko <amrreko@yahoo.com>
2022-05-17 14:16:09 +0200

    V5 Azan code telegrambot with laravel

--- app/Telegram/Commands/HelpCommand.php
+++ app/Telegram/Commands/HelpCommand.php
@@ -52,8 +52,10 @@ class HelpCommand extends Command
       
         for($i=0;$i<=5;$i++){
             // $text .= $this->getPrayerName($i) .' توقيت '.  $times[$i].chr(10);
+            if(!in_array($i,[1,5])){
             $azan = str_replace(['PM','AM'],['مساءً','صباحًا'],Carbon::parse($times[$i])->locale('ar')->format('g:i A'));
             $text .= $this->getPrayerName($i) .' توقيت '. $azan.chr(10);
+            }
         }
     
         $this->replyWithMessage(compact('text'));
